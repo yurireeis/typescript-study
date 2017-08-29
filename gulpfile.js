de6047932e0789eac6dcb9ba1512a72a98b5469a
Yuri Reis <yuri.reis@msn.com>
2017-08-28 22:41:57 -0300

    Close to start

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,22 @@
 
 const gulp = require('gulp');
 const clean = require('gulp-clean');
+const ts = require('gulp-typescript');
 const browserSync = require('browser-sync');
+
+const tsProject = ts.createProject({
+    declaration: true,
+    noImplicitAny: true,
+    
+});
 
 const APPROOT = 'app/';
 const SRCROOT = 'src/';
 
 const SRCPATH = {
     html: SRCROOT + 'views/',
-    js: [
+    ts: [
+        SRCROOT,
         SRCROOT + 'config/',
         SRCROOT + 'controllers/',
         SRCROOT + 'directives/',
@@ -30,15 +38,25 @@ gulp.task('html', ['clean-html'], function () {
 });
 
 gulp.task('clean-html', function () {
-   return gulp.src(APPROOT + '*.html', {
+    return gulp.src(APPROOT + '*.html', {
        read: false,
        force: true
-   }).pipe(clean());
+    }).pipe(clean());
 });
 
-gulp.task('watch', ['html'], function () {
+gulp.task('ts-output', function () {
+    var SRC = SRCPATH.ts.map(function (t) { return t + '*.ts' });
+
+    var tsResult =  gulp.src(SRC)
+        .pipe(tsProject());
+
+    return tsResult.js.pipe(gulp.dest(APPPATH.js))
+});
+
+gulp.task('watch', ['html', 'ts-output'], function () {
     gulp.watch([SRCPATH.html + '*.html'], ['html']);
+    gulp.watch([SRCPATH.ts + '*.ts'], ['ts-output']);
 });
 
-gulp.task('default', ['html']);
+gulp.task('default', ['html', 'ts-output']);
 gulp.task('dev', ['watch']);
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+/// <reference path="_all.ts" />
+import {module} from 'angular';
+
+/**
+ * The main typescript/angular study module
+ *
+ * @type {angular.module}
+ */
+
+let app = module('app', []);
+export = app;
