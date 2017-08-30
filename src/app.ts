0a459bcdd8049e61b082f65b2d3227863b78609d
Yuri Reis <yuri.reis@msn.com>
2017-08-30 14:57:42 -0300

    Recover app.ts file

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+/// <reference path="_all.ts" />
+import {module} from 'angular';
+
+/**
+ * The main typescript/angular study module
+ *
+ * @type {angular.IModule}
+ */
+
+let app = module('app', []);
+export = app;
